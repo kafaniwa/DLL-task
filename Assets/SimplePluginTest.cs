ba49400e6fbb20e310d0052d56c395f3159854ec
Alvin <alvin.aniwa@ontariotechu.net>
2019-09-27 19:11:08 -0400

    Initial commit

---
+++ Assets/SimplePluginTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+public class SimplePluginTest : MonoBehaviour
+{
+    const string DLL_NAME = "TESTPLUGIN";
+    [DllImport(DLL_NAME)]
+    private static extern void SimpleSaveFunction(float x, float y, float z);
+    [DllImport(DLL_NAME)]
+    private static extern float SimpleLoadFunctionX();
+    [DllImport(DLL_NAME)]
+    private static extern float SimpleLoadFunctionY();
+    [DllImport(DLL_NAME)]
+    private static extern float SimpleLoadFunctionZ();
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+
+            SimpleSaveFunction(transform.position.x, transform.position.y, transform.position.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            
+            transform.position = new Vector3(SimpleLoadFunctionX(), 0.5f, SimpleLoadFunctionZ());
+        }
+    }
+}
---
+++ Assets/cmovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cmovement : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0, vertical);
+
+        gameObject.transform.Translate(direction.normalized * Time.deltaTime * speed);
+    }
+}
